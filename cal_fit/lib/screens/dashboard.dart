8b20a8f0ad89a1b52cf65e5baa04d1edb5f4b1c0
Samarth Gupta <samarthgupta1202@gmail.com>
2023-04-12 12:20:46 +0530

    Step counter code added

--- cal_fit/lib/screens/dashboard.dart
+++ cal_fit/lib/screens/dashboard.dart
@@ -1,5 +1,8 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, unused_field
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, unused_field, annotate_overrides
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
 
 class dashboard extends StatefulWidget {
   const dashboard({super.key});
@@ -9,17 +12,39 @@ class dashboard extends StatefulWidget {
 }
 
 class _dashboardState extends State<dashboard> {
+  late StreamSubscription<StepCount> _subscription;
+
+  void initState() {
+    super.initState();
+    print("A stream was initialised");
+    // Initialize a stream to receive step count updates
+    _subscription = Pedometer.stepCountStream.listen(_onStepCount);
+  }
+
+  void dispose() {
+    super.dispose();
+    print("Stream was disposed");
+    // Cancel the step count stream subscription
+    _subscription.cancel();
+  }
+
+  int _stepCount = 0;
+
+  Future<int> _onStepCount(StepCount stepCount) async {
+    int count = stepCount.steps;
+    print("onStepCount function called");
+    setState(() {
+      _stepCount = count;
+    });
+    return _stepCount;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Step Counter'),
-      ),
-      body: Center(
-        child: Text(
-          'Step Count',
-          style: TextStyle(fontSize: 24),
-        ),
+    return Center(
+      child: Text(
+        'Step count: $_stepCount',
+        style: TextStyle(fontSize: 24.0),
       ),
     );
   }
