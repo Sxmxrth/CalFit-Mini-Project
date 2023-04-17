75c821ea9a485cd99b4d65ba519080b0ed7d3e65
Samarth Gupta <samarthgupta1202@gmail.com>
2023-04-17 21:24:49 +0530

    Shared preferences dependancy added

--- cal_fit/lib/screens/dashboard.dart
+++ cal_fit/lib/screens/dashboard.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, unused_field, annotate_overrides
 import 'dart:async';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
 
