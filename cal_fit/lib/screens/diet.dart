c465eb41cc6614c913c466df196cc705a89f6cb1
Samarth Gupta <samarthgupta1202@gmail.com>
2023-04-22 20:56:59 +0530

    Diet plan cards added

--- cal_fit/lib/screens/diet.dart
+++ cal_fit/lib/screens/diet.dart
@@ -1,17 +1,251 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class Diet extends StatelessWidget {
+class Diet extends StatefulWidget {
   const Diet({super.key});
 
   @override
+  State<Diet> createState() => _DietState();
+}
+
+class _DietState extends State<Diet> {
+  bool isExpanded1 = false;
+  bool isExpanded2 = false;
+  bool isExpanded3 = false;
+  bool isExpanded4 = false;
+  bool isExpanded5 = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Diet"),
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 80,
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xff19376D),
+        title: Text(
+          "Diets for you",
+          style: TextStyle(
+            fontFamily: "SourceSansPro",
+            fontSize: 35,
+          ),
+        ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+        children: [
+          Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isExpanded1 = !isExpanded1;
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.blue[200],
+                      image: DecorationImage(
+                        image: AssetImage("images/keto.avif"),
+                        fit: BoxFit.cover,
+                      )),
+                  width: double.infinity,
+                  height: isExpanded1 ? 190 : 120,
+                  child: isExpanded1
+                      ? Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Card 1 expanded",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isExpanded2 = !isExpanded2;
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.blue[200],
+                      image: DecorationImage(
+                        image: AssetImage("images/paleo.avif"),
+                        fit: BoxFit.cover,
+                      )),
+                  width: double.infinity,
+                  height: isExpanded2 ? 190 : 120,
+                  child: isExpanded2
+                      ? Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Card 2 expanded",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isExpanded3 = !isExpanded3;
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.blue[200],
+                      image: DecorationImage(
+                        image: AssetImage("images/vegan.avif"),
+                        fit: BoxFit.cover,
+                      )),
+                  width: double.infinity,
+                  height: isExpanded3 ? 190 : 120,
+                  child: isExpanded3
+                      ? Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Card 3 expanded",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isExpanded4 = !isExpanded4;
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.blue[200],
+                      image: DecorationImage(
+                        image: AssetImage("images/dukan.avif"),
+                        fit: BoxFit.cover,
+                      )),
+                  width: double.infinity,
+                  height: isExpanded4 ? 190 : 120,
+                  child: isExpanded4
+                      ? Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Card 4 expanded",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isExpanded5 = !isExpanded5;
+                  });
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: Colors.blue[200],
+                      image: DecorationImage(
+                        image: AssetImage("images/atkin.jpg"),
+                        fit: BoxFit.cover,
+                      )),
+                  width: double.infinity,
+                  height: isExpanded5 ? 190 : 120,
+                  child: isExpanded5
+                      ? Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Card 5 expanded",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Text(
+                            "Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
